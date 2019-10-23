66388abee274bed52af65008066388efb01ed5af
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 16:00:13 +0800

    質數判斷

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner a=new Scanner(System.in);
-        float z=a.nextFloat();
-        float max=z;
-        float min=z;
-        for (int s=1;s<10;s++){
-            z=a.nextFloat();
-            if (max<z){
-                max=z;
+        Scanner a = new Scanner(System.in);
+        int s = a.nextInt();
+        int d = s / 2;
+        boolean flag = true;
+        for (int x = 2; x <= d; x++) {
+            if (s % x == 0) {
+                flag = false;
             }
-            else if (min>z){
-                min=z;
+                if (flag) {
+                    System.out.println("yes");
+                } else {
+                    System.out.println("no");
+                }
             }
         }
-        System.out.printf("max=%.2f\n",max);
-        System.out.printf("min=%.2f\n",min);
     }
-}
+
