d8958b29f27357a272893727dada721cd6d23212
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-30 14:42:05 +0800

    '*'正三角形

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,10 @@ public class Main {
         Scanner a=new Scanner(System.in);
         int n=a.nextInt();
         for (int s=1;s<=n;s++){
-            for (int d=1;d<=s;d++){
+            for(int d=n;d>s;d--) {
+                System.out.print(" ");
+            }
+                for (int f=1;f<=2*s-1;f++){
                 System.out.print("*");
             }
             System.out.println();
