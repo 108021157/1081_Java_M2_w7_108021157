07b7e9a04ecb74f864723ec3ac5035f4271dbde2
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-06 16:05:32 +0800

    尋找連續整數相加

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
         int s=a.nextInt();
-        int z,x;
-        z=s;
-        x=s-1;
-        for(int d=1;d<x;x--){
-            z*=x;
+        int sum=0,d=0;
+        String str="";
+        for (int z=1;z<s;z++){
+            d=z;
+            str="";
+            sum=0;
+            while (sum<s && d<s){
+                sum+=d;
+                if(sum==s){
+                    str=str+"+"+Integer.toString(d);
+                    System.out.println(str);
+                }
+                else {
+                    if(z==d){
+                        str=Integer.toString(d);
+                    }
+                    else {
+                        str=str+"+"+Integer.toString(d);
+                    }
+                    d++;
+                }
+            }
         }
-        System.out.println(z);
     }
 }
