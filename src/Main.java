7c51c56f0800f96959fcb3b9a01f746b432e6fca
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-06 15:22:29 +0800

    n階乘

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
-        long s=a.nextLong();
-        int sum=0;int d=0;
-        while (s>0){
-            d++;
-            sum+=s%10;
-            s=s/10;
+        int s=a.nextInt();
+        int z,x;
+        z=s;
+        x=s-1;
+        for(int d=1;d<x;x--){
+            z*=x;
         }
-        System.out.println(d);
-        System.out.println(sum);
+        System.out.println(z);
     }
 }
