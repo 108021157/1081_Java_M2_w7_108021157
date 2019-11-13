d559632b7ce4f3d7f71b06c808304f33ecad7fba
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-13 13:40:17 +0800

    數列1

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner a = new Scanner(System.in);
+        Scanner a=new Scanner(System.in);
         int s=a.nextInt();
-        int sum=0,d=0;
-        String str="";
-        for (int z=1;z<s;z++){
-            d=z;
-            str="";
-            sum=0;
-            while (sum<s && d<s){
-                sum+=d;
-                if(sum==s){
-                    str=str+"+"+Integer.toString(d);
-                    System.out.println(str);
-                }
-                else {
-                    if(z==d){
-                        str=Integer.toString(d);
-                    }
-                    else {
-                        str=str+"+"+Integer.toString(d);
-                    }
-                    d++;
-                }
-            }
+        int z=1,x=1;
+        while (z<s){
+            System.out.print(z+"\t");
+            z=z+x;
+            x++;
         }
+        System.out.println(z);
     }
 }
